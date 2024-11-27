8ce78984da8289e20655722cd6899b4e2bc42f85
Sergey <dersergeant@gmail.com>
2024-11-27 13:50:51 +0300

    modified:   OP04/OP04_1.py modified:   OP04/OP04_2.py modified:   OP04/OP04_3.py

--- OP04/OP04_1.py
+++ OP04/OP04_1.py
@@ -1,4 +1,10 @@
+# Напишите функцию sum_range(start, end), которая суммирует 
+# все целые числа от значения start до величины end включительно
+
+
 # Объявление функции
+
+
 def sum_range(start, end):
     result = 0
     for i in range(start, end + 1):
@@ -6,7 +12,11 @@ def sum_range(start, end):
     return result
 
 # Ввод переменных
+
+
 a, b = int(input('Введите начало диапазона: ')), int(input('Введите конец диапазона: '))
 
 # Вывод результата
+
+
 print(f'Сумма чисел в диапазоне от {a} до {b} равна {sum_range(a, b)}')  
